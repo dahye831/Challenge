af855eb0b501ff6447c8805ca6485766655efda5
dahyehan <dahyehan31@gmail.com>
2023-05-05 23:24:26 +0900

    fix : todo 데이터 저장

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -3,13 +3,14 @@ const todoForm = document.querySelector('.todo__form');
 const todoInput = document.querySelector(".todo__input");
 const todoList = document.querySelector('.todo__list')
 
+const TODO_KEY = 'todo'
 let todoArray = [];
 
-const todoPaint = (key, value) => {
+const todoPaint = (todoObj) => {
   //li 생성
   const todoItem = document.createElement("li");
-  todoItem.id = key;
-  todoItem.innerText = value;
+  todoItem.id = todoObj.id;
+  todoItem.innerText = todoObj.item;
   todoList.appendChild(todoItem);
 
   //delete button 생성
@@ -23,42 +24,38 @@ const todoPaint = (key, value) => {
 
 const todoDelete = (event) => {
   const target = event.target.parentNode;
-  const targetId = target.getAttribute("id");
 
-  target.remove();
-  localStorage.removeItem(targetId);
+  target.remove();//dom 제거
+  todoArray = todoArray.filter((item) => item.id !== parseInt(target.id));
+  todoSave()//localStorage 수정
 };
 
-const todoSave = (key, value) => {
-  localStorage.setItem(key, value);
-
-  const todoObj = {
-    id: key,
-    item: value,
-  };
-  todoArray.push(todoObj);
+const todoSave = () => {
+  localStorage.setItem(TODO_KEY, JSON.stringify(todoArray));
 };
 
 const onSubmitTodo = (e) => {
   e.preventDefault();
 
-  const TODO_KEY = Date.now();
-  const newTodo = todoInput.value;
-  todoInput.value = '';
-  
-  todoSave(TODO_KEY, newTodo);
-  todoPaint(TODO_KEY, newTodo);
+  const todoObj = {
+    id: Date.now(),
+    item: todoInput.value,
+  };
+  todoArray.push(todoObj);
+  todoPaint(todoObj);
+  todoSave();
+
+  todoInput.value = "";
 }
 
 todoForm.addEventListener("submit", onSubmitTodo);
 
+const savedTodo = localStorage.getItem(TODO_KEY)
+// localStorage 데이터는 문자열임
 
-
-if (localStorage.getItem !== null) {
-  for (let i = 0; localStorage.length > i; i++) {
-    const localStorageId = localStorage.key(i);
-    const localStorageValue = localStorage.getItem(localStorageId);
-
-    todoPaint(localStorageId, localStorageValue);
-  }
+if (savedTodo !== null) {
+  console.log(savedTodo)
+  const todoData = JSON.parse(savedTodo)
+  todoArray = todoData; //저장한 데이터에 새로운 데이터를 추가하기 위해
+  todoData.map(item => todoPaint(item))
 }
