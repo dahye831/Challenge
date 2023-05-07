c104b9a37ff8af47fb57a4eac8af45aa500398c2
dahyehan <dahyehan31@gmail.com>
2023-05-07 20:05:05 +0900

    clone project 삭제

--- chromeApp/js/background.js
+++
@@ -1,8 +0,0 @@
-const images = ["bg-0.jpeg", "bg-1.jpeg", "bg-2.jpeg"];
-
-const chooseImages = images[Math.floor(Math.random() * images.length)];
-
-const imageElement = document.createElement('img');
-imageElement.src = `./img/${chooseImages}`
-
-document.body.appendChild(imageElement)
