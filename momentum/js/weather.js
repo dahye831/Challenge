2f12208cfd50280345bd3b487df8e481eaac49e9
한다혜/글로벌개발팀 <dahye.han@musinsa.com>
2023-05-07 21:29:09 +0900

    fix : weather 날씨 수정

--- momentum/js/weather.js
+++ momentum/js/weather.js
@@ -1,29 +1,26 @@
-
 function success(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
 
   const API_KEY = "eec4f33e29af69a6a4ae53075f4cb632";
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`;
-  console.log(url)
+  console.log(url);
 
-  const weatherData = document.querySelectorAll('.data');
-  console.log(weatherData[0])
+  const weatherData = document.querySelectorAll(".data");
+  console.log(weatherData[0]);
 
   fetch(url)
     .then((response) => response.json())
-    .then((data) => 
-      {    
-        const city = data.name;
-        const weather = data.weather.main;
-        const temp = data.main.temp;
-        const dataArray = [city, weather, temp]
-        
-        for (let i = 0; i < weatherData.length; i++) {
-          weatherData[i].innerHTML = dataArray[i]
-        }
+    .then((data) => {
+      const city = data.name;
+      const weather = data.weather[0].main;
+      const temp = data.main.temp;
+      const dataArray = [city, weather, temp];
+
+      for (let i = 0; i < weatherData.length; i++) {
+        weatherData[i].innerHTML = dataArray[i];
       }
-    );
+    });
 }
 function error(error) {
   alert(`ERROR(${error.code}): ${error.message}`);
