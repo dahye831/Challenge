ecb468241e68ccb65c724d9f50fddcf92c30a01a
dahyehan <dahyehan31@gmail.com>
2023-05-25 23:27:44 +0900

    build & publish

--- movie-app/src/routes/details.js
+++ movie-app/src/routes/details.js
@@ -7,22 +7,28 @@ const Details = () => {
   const [loading, setLoading] = useState(false);
   const [item, setItem] = useState('');
 
-  const getMovie = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
-    ).json();
-    setLoading(true)
-    setItem(json.data.movie)
-  }
+  // const getMovie = async () => {
+  //   const json = await (
+  //     await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+  //   ).json();
+  //   console.log('json')
+  //   setLoading(true)
+  //   setItem(json.data.movie)
+  // }
    useEffect(() => {
-     getMovie()
+    //  getMovie()
+     fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+       .then((response) => response.json())
+       .then((json) => setItem(json.data.movie));
+     setLoading(true);
+    //  console.log(item);
+     console.log(2)
    }, []);
-  console.log(item)
+  console.log(1)
   return (
     <>
-      {!loading ? (
-        <h1>Loading...</h1>
-      ) : (
+      {!loading ? <h1>Loading...</h1> : ''}
+      {item && (
         <MovieDetail
           title={item.title}
           genres={item.genres}
