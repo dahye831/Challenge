048ae50fc684804f1165349177f843391627c975
dahyehan <dahyehan31@gmail.com>
2023-05-29 01:12:12 +0900

    상태관리 useImmer 라이브러리 적용

--- mentor/README.md
+++ mentor/README.md
@@ -7,7 +7,6 @@
 - 리액트의 객체 불변성
   - 변경된 데이터를 병합하지 않고 복사한다. 
   - 참고 : [https://velog.io/@rimo09/react-%EB%B6%88%EB%B3%80%EA%B0%9D%EC%B2%B4%EB%A5%BC-%EC%82%AC%EC%9A%A9%ED%95%B4%EC%95%BC-%ED%95%98%EB%8A%94-%EC%9D%B4%EC%9C%A0]
-
 - statement & expression
   - expression : 값으로 표현될 수 있는 문
   - statement : 프로그램을 구성하는 기본 단위이자 최소 실행 단위
@@ -27,7 +26,6 @@
     const state = variableState[0]
     const setState = variableState[1]
   </code></pre>
-
 - useReducer : [https://ko.legacy.reactjs.org/docs/hooks-reference.html#usereducer]
 <pre>
 <code>
@@ -44,5 +42,6 @@ function reducer(state, action) {
 }
 </code>
 </pre>
+- useImmer : [https://github.com/immerjs/use-immer]
 - 비교 알고리즘
   - Object.is() [https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object/is#Description]
--- mentor/src/components/Mentors.js
+++ mentor/src/components/Mentors.js
@@ -1,5 +1,5 @@
-import React, { useReducer} from "react";
-import mentorReducer from "../reducer/mentors-reducer";
+import React, { useState } from "react";
+import { useImmer } from "use-immer";
 
 const initData = [
   { name: "Ria", position: "developer" },
@@ -8,42 +8,58 @@ const initData = [
 ];
 
 const Mentors = () => {
-  const [mentor, dispatch] = useReducer(mentorReducer, initData);
-
+  const [person, updatePerson] = useImmer(initData)
   const handleChange = () => {
-    const prev = prompt('이름을 바꿀 멘토는 누구입니까?')
-    const current = prompt('바꿀 이름을 입력해주세요')
-    
-    dispatch({type: 'changed', prev, current})
-  }
+    const prev = prompt("이름을 바꿀 멘토는 누구입니까?");
+    const current = prompt("바꿀 이름을 입력해주세요");
+
+    updatePerson(person => {
+      const mentor = person.find(item => item.name === prev)
+      mentor.name = current
+    })
+  };
 
   const handleAdd = () => {
-    const name = prompt('추가할 멘토의 이름을 입력해주세요')
-    const position = prompt('멘토의 직업을 입력해주세요')
+    const name = prompt("추가할 멘토의 이름을 입력해주세요");
+    const position = prompt("멘토의 직업을 입력해주세요");
 
-    dispatch({ type: "added", name, position });
-  }
+    updatePerson(person => {
+      person.push({name, position})
+    })
+  };
 
   const handleDelete = () => {
-    const name = prompt("삭제할 멘토의 이름을 입력해주세요")
-    
-    dispatch({ type: "deleted", name });
-  }
+    const name = prompt("삭제할 멘토의 이름을 입력해주세요");
+
+    updatePerson(person => {
+      const index = person.findIndex(item => item.name === name)
+      person.splice(index, 1);
+    })
+
+  };
 
   return (
     <>
       <h1>Mentors</h1>
       <ul>
-        {mentor.map((item, index) => (
-          <li key={`mentor-${index}`}>{item.name} ({item.position})</li>
+        {person.map((item, index) => (
+          <li key={`mentor-${index}`}>
+            {item.name} ({item.position})
+          </li>
         ))}
       </ul>
 
-      <button type="button" onClick={handleChange}>멘토 이름 바꾸기</button>
-      <button type="button" onClick={handleAdd}>멘토 추가하기</button>
-      <button type="button" onClick={handleDelete}>멘토 삭제하기</button>
+      <button type="button" onClick={handleChange}>
+        멘토 이름 바꾸기
+      </button>
+      <button type="button" onClick={handleAdd}>
+        멘토 추가하기
+      </button>
+      <button type="button" onClick={handleDelete}>
+        멘토 삭제하기
+      </button>
     </>
   );
-}
+};
 
-export default Mentors
+export default Mentors;
