fc908c8766c1cf28e7754eda52853aba1309f222
dahyehan <dahyehan31@gmail.com>
2023-05-30 22:56:35 +0900

    feat : todo 입력 및 삭제

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,41 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoItem from "./components/TodoItem";
-
-const data = ["abcd", "Dyfed", "안녕하세요"];
 
 function App() {
   const [value, setValue] = useState("");
   const [todoList, setTodoList] = useState([]);
 
   const handleChange = (e) => {
-    console.log(e);
     const inputValue = e.target.value;
-    console.log(inputValue);
     setValue(inputValue);
   };
+
+  const handleSubmit = () => {
+    setTodoList([...todoList, value]);
+    setValue('')
+  }
+
+  const handleDelete = (e) => {
+    console.log(e)
+    const deleteText = e.target.previousSibling.innerText
+    setTodoList(todoList.filter((todo) => todo !== deleteText));
+  }
+
+  console.log(todoList)
   return (
     <div className="App">
-      <h1>TODO LIST</h1>
-      <div>
+      <h1>오늘의 할일</h1>
+      <h2>
+        Today is <time>시간</time>
+      </h2>
+      <article>
         <h2>일정을 입력하세요</h2>
         <div className="todo-input">
           <input
             type="text"
             placeholder="일정을 입력하세요"
-            // onClick={handleClick}
             onChange={handleChange}
             value={value}
           />
+          <button type="button" onClick={handleSubmit}>
+            입력
+          </button>
         </div>
-      </div>
-      <ul>
-        {/* {data.map((item, index) => (
-          <li key={`todo-${index}`}>
-            <TodoItem id={`checkbox-${index}`} text={item} />
-          </li>
-        ))} */}
-        <TodoItem id={"checkbox1"} text={value} />
-      </ul>
+        <section>
+          <h3>Todo List</h3>
+          <ul>
+            {todoList.map((item, index) => (
+              <li key={`todo-${index}`}>
+                <div className="todo-item">
+                  <input
+                    type="checkbox"
+                    id={`checkbox-${index}`}
+                    name="todo-check"
+                  />
+                  <label for={`checkbox-${index}`}>{item}</label>
+                  <button type="button" onClick={handleDelete}>
+                    삭제
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </article>
     </div>
   );
 }
